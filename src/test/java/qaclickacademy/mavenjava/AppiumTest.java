451f81c50f6d1f8fe86228244b8c398ee877d5e4
nived <nivedsr@gmail.com>
2021-01-07 17:12:37 +1100

    first commit

---
+++ src/test/java/qaclickacademy/mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void appLogin() {
+		System.out.println("App login is success");
+		
+	}
+	
+	@Test
+	public void appLogout() {
+		System.out.println("App logout is success");
+		
+	}
+
+}
---
+++ src/test/java/qaclickacademy/mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void userLogin() {
+		System.out.println("Login is success");
+	}
+	
+	@Test
+	public void userLogout() {
+		System.out.println("Logout is success");
+	}
+
+}
